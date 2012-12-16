24246ec20ff1fcd37fa1c82115e9a24c49990bc7
Eric Zetterbaum <eric@zetterbaum.com>
2012-12-16 07:09:06 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ezquiz::Application.config.secret_token = '0ca099fb8745a405e9c23342dd3a878bb8c97f0b0e52b4e200ffdbdaf66bc0a366a27087c39dee90579138390d937463da3cc02f8b39920020f84d8008143775'
